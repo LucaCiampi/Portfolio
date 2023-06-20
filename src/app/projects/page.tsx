1bf6d106accc627338476e8116edc7e04680d305
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-06-20 13:30:51 +0200

    ✨ Added projects list on /projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,18 @@
 import projectsData from 'json/projects.json';
-import Image from 'next/image'
 import PageWrapper from '../page-wrapper'
+import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <PageWrapper>
       <div>
         projects
+        {projectsData.map((project) => (
+          <Link href={'/projects/' + project.slug}>
+            <div>{project.title}</div>
+          </Link>
+        ))}
       </div>
     </PageWrapper>
   )
