a33e3b7c52b12035b9a17f75384bf30e66bccded
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-06-20 15:01:38 +0200

    ✨ Added NextJS13 optimized fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Playfair_Display, Abril_Fatface } from 'next/font/google'
 import Header from './header'
 import DarkModeToggle from '../components/DarkModeToggle'
 
 import '@/styles/globals.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+
+const abril_fatface = Abril_Fatface({
+  subsets: ['latin'],
+  weight: "400",
+  variable: '--font-abril-fatface',
+  display: 'swap',
+})
+
+const playfair_display = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-playfair-display',
+  display: 'swap',
+})
+
 
 export const metadata = {
   title: 'Portfolio 2023',
@@ -19,7 +32,7 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="fr">
-      <body className={inter.className}>
+      <body className={`${abril_fatface.variable} ${playfair_display.variable}`}>
         <Header />
         <DarkModeToggle />
         {children}
