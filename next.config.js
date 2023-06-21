4bc0c553788de7be152f2966a24866f0e944439e
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-06-21 09:41:44 +0200

    ➕ Added SVGR

--- next.config.js
+++ next.config.js
@@ -2,11 +2,20 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-    // trailingSlash: true,
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  };
-   
-  module.exports = nextConfig;
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+
+  // SVGR
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/components/ExternalLinkSocial.tsx
+++ src/components/ExternalLinkSocial.tsx
@@ -1,4 +1,6 @@
 import ExternalLink from "./ExternalLink"
+import Github from "public/images/social/github.svg"
+import Linkedin from "../../public/images/social/linkedin.svg"
 import Image from "next/image"
 
 interface Props {
@@ -14,12 +16,24 @@ const ExternalLinkSocial = ({ name, href, className }: Props) => {
     const properName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
     const lowercaseName = name.toLowerCase();
     const svgPath = `/images/social/${lowercaseName}.svg`;
+    let icon = <></>
+
+    switch (lowercaseName) {
+        case 'github':
+            icon = <Github />
+            break;
+        case 'linkedin':
+            icon = <Linkedin />
+            break;
+        default:
+            icon = <></>
+            break;
+    }
 
     return (
         <ExternalLink href={href} className={className || ''}>
-            <object type="image/svg+xml" data={svgPath}>
-                <span>Votre navigateur ne prend pas en charge les SVG.</span>
-            </object>
+            {icon}
+            <span title={properName}>{properName}</span>
         </ExternalLink>
     )
 }
