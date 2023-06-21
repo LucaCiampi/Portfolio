c8429eb37a0e33ca619908b012ebcbcfb600b8a3
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-06-21 09:45:00 +0200

    :zap: minor adjustments on ExternalLinkSocial

--- src/components/ExternalLinkSocial.tsx
+++ src/components/ExternalLinkSocial.tsx
@@ -1,38 +1,21 @@
 import ExternalLink from "./ExternalLink"
 import Github from "public/images/social/github.svg"
-import Linkedin from "../../public/images/social/linkedin.svg"
+import Linkedin from "public/images/social/linkedin.svg"
 import Image from "next/image"
 
 interface Props {
     name: string,
     href: string,
     className?: string,
-    outline?: boolean,
-    white?: boolean,
-    size?: number,
 }
 
 const ExternalLinkSocial = ({ name, href, className }: Props) => {
     const properName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-    const lowercaseName = name.toLowerCase();
-    const svgPath = `/images/social/${lowercaseName}.svg`;
-    let icon = <></>
-
-    switch (lowercaseName) {
-        case 'github':
-            icon = <Github />
-            break;
-        case 'linkedin':
-            icon = <Linkedin />
-            break;
-        default:
-            icon = <></>
-            break;
-    }
+    const IconComponent = name === "github" ? Github : name === "linkedin" ? Linkedin : null;
 
     return (
         <ExternalLink href={href} className={className || ''}>
-            {icon}
+            {IconComponent && <IconComponent />}
             <span title={properName}>{properName}</span>
         </ExternalLink>
     )
