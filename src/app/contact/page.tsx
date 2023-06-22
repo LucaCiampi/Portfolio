5eba472b39b0b0affcf8d3884b8f2d10d6403b78
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-06-22 11:47:44 +0200

    ➕ Added Lottie

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import ExternalLinkSocial from '@/components/ExternalLinkSocial'
 import PageWrapper from '../page-wrapper'
+import Lottie from "lottie-react";
+import Cheers from "public/lotties/cheers.json";
 
 export default function Page() {
   return (
     <PageWrapper>
       <div>
         <h1>Contact</h1>
+        <div className="w-96">
+          <Lottie animationData={Cheers} />
+        </div>
         <div>
           <ExternalLinkSocial name={'github'} href={'https://github.com/LucaCiampi'} />
           <ExternalLinkSocial name={'linkedin'} href={'https://linkedin.com/in/lucaciampi'} />
