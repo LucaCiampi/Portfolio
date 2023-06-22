9cd4930ec73c48b524e407a6f70f13f661a12fab
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-06-22 23:09:14 +0200

    🎨 updated noscrolllink component file naming and position

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react'
 import React from 'react'
 import { LayoutGroup } from 'framer-motion'
 
-import NoScrollLink from './no-scroll-link'
+import NoScrollLink from '@/components/NoScrollLink'
 import DarkModeToggle from '@/components/DarkModeToggle';
 
 interface Link {
