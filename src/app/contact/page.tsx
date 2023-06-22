87287a2c8949df2081856cbb7ea434a5d40c9c03
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-06-22 12:01:49 +0200

    ✨ Added button to play lottie

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,18 +4,28 @@ import ExternalLinkSocial from '@/components/ExternalLinkSocial'
 import PageWrapper from '../page-wrapper'
 import Lottie from "lottie-react";
 import Cheers from "public/lotties/cheers.json";
+import { useRef } from 'react';
 
 export default function Page() {
+  const lottieRef = useRef(null);
+
+  function handleLottiePlayButtonClick(): void {
+    if (lottieRef.current) {
+      lottieRef.current.play();
+    }
+  }
+
   return (
     <PageWrapper>
       <div>
         <h1>Contact</h1>
         <div className="w-96">
-          <Lottie animationData={Cheers} />
+          <Lottie autoplay={false} loop={false} lottieRef={lottieRef} animationData={Cheers} />
         </div>
         <div>
           <ExternalLinkSocial name={'github'} href={'https://github.com/LucaCiampi'} />
           <ExternalLinkSocial name={'linkedin'} href={'https://linkedin.com/in/lucaciampi'} />
+          <button onClick={handleLottiePlayButtonClick}>PLAY LOTTIE</button>
         </div>
       </div>
     </PageWrapper>
