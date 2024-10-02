b2613e29320be867500c29fe1be4dea5215b9973
Luca Ciampi <91676870+LucaCiampi@users.noreply.github.com>
2024-10-02 14:54:52 +0200

    💄 adjustments in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,18 +4,18 @@ import Navigation from '@/components/Navigation';
 import Heading from '@/components/Heading';
 
 const Footer = () => (
-  <footer className="bg-green text-background py-10 md:pt-20 px-6 md:px-0">
-    <Container className="flex flex-wrap justify-between">
+  <footer className="bg-green text-background py-8 md:pt-20 px-6 md:px-0">
+    <Container className="flex flex-wrap justify-between mb-10">
       <div className="w-full md:w-max">
-        <Image src="/images/rose.svg" alt="rose" width={153} height={270.6} />
+        <Image src="/images/rose.svg" alt="rose" width={107} height={240} />
       </div>
-      <div className="w-full md:w-max">
+      <div className="w-full md:w-max"></div>
+      <div className="w-full md:w-max text-right">
         <Heading level={'h3'} className="mb-4">
           Navigation
         </Heading>
         <Navigation ulClassName="flex-col !gap-2" />
       </div>
-      <div className="w-full md:w-max">item</div>
     </Container>
     <Container className="text-right text-xs px-6 md:px-0">
       Design & development by Luca Ciampi
