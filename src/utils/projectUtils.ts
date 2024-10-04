8996514af018b6f502890f6259be1cc0f2f65484
Luca Ciampi <91676870+LucaCiampi@users.noreply.github.com>
2024-10-04 22:27:22 +0200

    📝 added JSDoc

--- src/utils/projectUtils.ts
+++ src/utils/projectUtils.ts
@@ -1,5 +1,10 @@
 /**
- * Retourne les projets qui correspondent aux technologies et à la chaîne de recherche
+ * Retourne les projets qui correspondent aux technologies et au terme de recherche.
+ *
+ * @param technologies - Liste des technologies à filtrer.
+ * @param searchTerm - Terme de recherche pour filtrer les projets.
+ * @param projectsData - Liste complète des projets.
+ * @returns Liste des projets filtrés.
  */
 export function getProjectsByTechnologyAndSearchTerm(
   technologies: string[],
@@ -20,7 +25,10 @@ export function getProjectsByTechnologyAndSearchTerm(
 }
 
 /**
- * Regroupe les projets par année
+ * Regroupe les projets par année.
+ *
+ * @param projects - Liste des projets à regrouper.
+ * @returns Un objet où chaque clé est une année et la valeur est un tableau de projets.
  */
 export function groupProjectsByYear(projects: Project[]): GroupedProjects {
   return projects.reduce((acc: GroupedProjects, project: Project) => {
@@ -34,7 +42,9 @@ export function groupProjectsByYear(projects: Project[]): GroupedProjects {
 }
 
 /**
- * Récupère les filtres depuis l'URL
+ * Récupère les filtres actifs depuis l'URL.
+ *
+ * @returns Une chaîne contenant les filtres actifs ou null s'il n'y en a pas.
  */
 export function getFiltersParamFromURL(): string | null {
   const queryString = window.location.search;
@@ -43,7 +53,9 @@ export function getFiltersParamFromURL(): string | null {
 }
 
 /**
- * Met à jour les filtres dans l'URL
+ * Met à jour les filtres actifs dans l'URL sans recharger la page.
+ *
+ * @param activeFilters - Liste des filtres actifs.
  */
 export function updateFiltersToURL(activeFilters: string[]): void {
   const filtersParam =
